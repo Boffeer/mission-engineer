6eeccd8f58f26d259b33ee55fb435f494b7c34a4
Boffeer <41575886+Boffeer@users.noreply.github.com>
2022-07-29 09:01:36 +0300

    Делает календарь

--- js/main.js
+++ js/main.js
@@ -863,64 +863,65 @@ initInputs(inputs);
 // #endregion input-labels
 
 // #region datepicker
-/*
-Datepicker.locales.ru = {
-  days: [
-    "Воскресенье",
-    "Понедельник",
-    "Вторник",
-    "Среда",
-    "Четверг",
-    "Пятница",
-    "Суббота",
-  ],
-  daysShort: ["Вск", "Пнд", "Втр", "Срд", "Чтв", "Птн", "Суб"],
-  daysMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
-  months: [
-    "Январь",
-    "Февраль",
-    "Март",
-    "Апрель",
-    "Май",
-    "Июнь",
-    "Июль",
-    "Август",
-    "Сентябрь",
-    "Октябрь",
-    "Ноябрь",
-    "Декабрь",
-  ],
-  monthsShort: [
-    "Янв",
-    "Фев",
-    "Мар",
-    "Апр",
-    "Май",
-    "Июн",
-    "Июл",
-    "Авг",
-    "Сен",
-    "Окт",
-    "Ноя",
-    "Дек",
-  ],
-  today: "Сегодня",
-  clear: "Очистить",
-  format: "dd.mm.yyyy",
-  weekStart: 1,
-  monthsTitle: "Месяцы",
-};
-Object.assign(Datepicker.locales);
-const calendarInputs = document.querySelectorAll(".input--calendar");
-if (calendarInputs) {
-  calendarInputs.forEach((calendar) => {
-    const datepicker = new Datepicker(calendar, {
-      format: "dd.mm.yyyy",
-      autohide: true,
-      maxDate: new Date(),
-      language: "ru",
+
+if (Datepicker) {
+  Datepicker.locales.ru = {
+    days: [
+      "Воскресенье",
+      "Понедельник",
+      "Вторник",
+      "Среда",
+      "Четверг",
+      "Пятница",
+      "Суббота",
+    ],
+    daysShort: ["Вск", "Пнд", "Втр", "Срд", "Чтв", "Птн", "Суб"],
+    daysMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+    months: [
+      "Январь",
+      "Февраль",
+      "Март",
+      "Апрель",
+      "Май",
+      "Июнь",
+      "Июль",
+      "Август",
+      "Сентябрь",
+      "Октябрь",
+      "Ноябрь",
+      "Декабрь",
+    ],
+    monthsShort: [
+      "Янв",
+      "Фев",
+      "Мар",
+      "Апр",
+      "Май",
+      "Июн",
+      "Июл",
+      "Авг",
+      "Сен",
+      "Окт",
+      "Ноя",
+      "Дек",
+    ],
+    today: "Сегодня",
+    clear: "Очистить",
+    format: "dd.mm.yyyy",
+    weekStart: 1,
+    monthsTitle: "Месяцы",
+  };
+  Object.assign(Datepicker.locales);
+  const calendarInputs = document.querySelectorAll(".input--calendar");
+  if (calendarInputs) {
+    calendarInputs.forEach((calendar) => {
+      const datepicker = new Datepicker(calendar, {
+        format: "dd.mm.yyyy",
+        autohide: true,
+        maxDate: new Date(),
+        language: "ru",
+      });
     });
-  });
+  }
 }
-*/
 // #endregion datepicker
